185663ef6a8b24e3b0aaeb6c164461208198a2a4
psy-choi <84831081+psy-choi@users.noreply.github.com>
2021-06-21 14:13:14 +0900

    index.js를 바꿈, 우리 교회 화면 설정
    index.js에서 영성보고 화면을 굳이 한 화면에 넣지 않고 php로 다른 화면으로 나타나도록 바꿨음.
    우리 교회 화면을 통해서 채팅하는 기능 추가.

--- index.js
+++ index.js
@@ -1,56 +1,97 @@
-// with-God
-//영성생활 체크를 위한 웹 사이트 개발
-var date = new Date();
-const viewYear = date.getFullYear();
-const viewMonth = date.getMonth();
-document.querySelector('#year_month').textContent = `${viewYear}년 ${viewMonth + 1}월`;
+let date = new Date();
 
-const prevLast = new Date(viewYear, viewMonth, 0);
-const thisLast = new Date(viewYear, viewMonth + 1, 0);
+const renderCalender = () => {
+  const viewYear = date.getFullYear();
+  const viewMonth = date.getMonth();
 
-const PLDate = prevLast.getDate(); //날
-const PLDay = prevLast.getDay(); //요일
+  document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth + 1}월`;
 
-const TLDate = thisLast.getDate();
-const TLDay = thisLast.getDay();
+  const prevLast = new Date(viewYear, viewMonth, 0);
+  const thisLast = new Date(viewYear, viewMonth + 1, 0);
 
-const prevDates = [];
-const thisDates = [...Array(TLDate + 1).keys()].slice(1); //slice를 통해 0을 없애줌. + keys는 해당 값 도출 즉 0부터 30까지 나
-const nextDates = [];
+  const PLDate = prevLast.getDate();
+  const PLDay = prevLast.getDay();
 
-if (PLDay !== 6) { //토요일까지 있으면 바로 주일부터 시작이기 때문에 전 달이 필요가 없음
-  for (let i = 0; i < PLDay + 1; i++) {
-    prevDates.unshift(PLDate - i);  //하나씩 뒤로 가는 거기 때문에
+  const TLDate = thisLast.getDate();
+  const TLDay = thisLast.getDay();
+
+  const prevDates = [];
+  const thisDates = [...Array(TLDate + 1).keys()].slice(1);
+  const nextDates = [];
+
+  if (PLDay !== 6) {
+    for (let i = 0; i < PLDay + 1; i++) {
+      prevDates.unshift(PLDate - i);
+    }
   }
-}
-for (let i = 1; i < 7 - TLDay; i++) {
-  nextDates.push(i);
-}
-const dates_arr = prevDates.concat(thisDates, nextDates) //배열을 합치는 함수
 
-
-for (var date of dates_arr) { //dddd
-  var d = document.querySelector('.dates');
-  d.insertAdjacentHTML('beforeend', `<div class="date" onclick="small_window();">`+date+`</div>`);
-  // dates[i] = `<div class="date" onclick="small_window()">${date}</div>`
-}
-
-var already = false
-function small_window() {
-  var ad = document.querySelector("aside");
-  if (already===false){
-    var in_t = '<form action="today.jsp" method="post">오늘 기도한 시간<br><input type="text" class="text" name="pray"></input><br>오늘 읽은 말씀<br>\
-    <input type="text" class="text" name="read"></input><br>\
-    큐티<br>o<input type="radio" name="QT" value="ok">\
-    x<input type="radio" name="QT" value="no"><br>\
-    오늘 느낀 점<br><textarea rows="30" cols="30" class="text" name="feel"></textarea><br>\
-    <input type="submit" value="Send"></form>'
-    ad.insertAdjacentHTML('beforeend', in_t);
-    already = true
+  for (let i = 1; i < 7 - TLDay; i++) {
+    nextDates.push(i);
   }
-  else{
-    ad.empty()
-    already = false
+
+  const dates = prevDates.concat(thisDates, nextDates);
+  const firstDateIndex = dates.indexOf(1);
+  const lastDateIndex = dates.lastIndexOf(TLDate);
+
+  dates.forEach((date, i) => {
+    const condition = i >= firstDateIndex && i < lastDateIndex + 1
+                      ? 'this'
+                      : 'other';
+    dates[i] = `<div class="date" onclick = "checking(${viewYear},${viewMonth + 1},${date});"><span class=${condition}>${date}</span></div>`;
+  });
+
+  document.querySelector('.dates').innerHTML = dates.join('');
+
+  const today = new Date();
+  if (viewMonth === today.getMonth() && viewYear === today.getFullYear()) {
+    for (let date of document.querySelectorAll('.this')) {
+      if (+date.innerText === today.getDate()) {
+        date.classList.add('today');
+        break;
+      }
+    }
   }
+};
+
+renderCalender();
+
+const prevMonth = () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalender();
+};
+
+const nextMonth = () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalender();
+};
+
+const goToday = () => {
+  date = new Date();
+  renderCalender();
+};
+
+// var already = false
+// function small_window() {
+//   var ad = document.querySelector("aside");
+//   if (already===false){
+//     var in_t = '<form action="today.jsp" method="post">오늘 기도한 시간<br><input type="text" class="text" name="pray"></input><br>오늘 읽은 말씀<br>\
+//     <input type="text" class="text" name="read"></input><br>\
+//     큐티<br>o<input type="radio" name="QT" value="ok">\
+//     x<input type="radio" name="QT" value="no"><br>\
+//     오늘 느낀 점<br><textarea rows="30" cols="30" class="text" name="feel"></textarea><br>\
+//     <input type="submit" value="Send"></form>'
+//     ad.insertAdjacentHTML('beforeend', in_t);
+//     already = true
+//   }
+//   else{
+//     ad.empty()
+//     already = false
+//   }
+// }
+function checking(a, b, c) {
+  location = "/checking.php?year="+a+"&month="+b+"&date="+c+""
 }
+
+
+
 document.querySelector('.dates').innerHTML = dates.join('');
